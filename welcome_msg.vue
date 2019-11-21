dcbf977ad992dec1bf0b49e16ad200c3c9709456
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-21 16:06:36 -0500

    Bowles Crossing 1.0  /welcome_msg.vue

---
+++ welcome_msg.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="property" class="main_container">
+        <div class="message_content">
+            <h1 class="center">{{ property.welcome_message }}</h1>
+            <p class="center">{{ property.description }}</p> 
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex"], function(Vue, Vuex){
+        return Vue.component("messages-component", {
+            template: template, // the variable template will be injected
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ])
+            }
+            console.log(property)
+        })
+    });
+</script>
