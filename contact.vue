8a904c0d7f6e517c16abfa44c45c30f123d835dc
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-21 14:58:21 -0500

    Bowles Crossing 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -127,8 +127,8 @@
                     console.log(response)
                     if(response && response[0]){
                         this.main = response[0].data
-                        if(response[0].data && response[0].data.subpages){
-                           this.contactInfo = response[0].data.subpages[0];
+                        if(response[0].data){
+                           this.contactInfo = response[0].data;
                         }
                         
                     }
