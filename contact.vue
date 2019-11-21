e6805102db6222136f1bc4d13108f5bb307771b5
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-21 14:52:01 -0500

    Bowles Crossing 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -129,6 +129,7 @@
                         if(response[0].data && response[0].data.subpages){
                            this.contactInfo = response[0].data.subpages[0];
                         }
+                        console.log(contactInfo)
                     }
                     this.dataLoaded = true;
                 });
