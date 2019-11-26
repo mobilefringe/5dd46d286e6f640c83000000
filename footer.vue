21934413d7d69e06388376a926819a5f2277e47f
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-26 16:37:45 -0500

    Bowles Crossing 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -27,7 +27,7 @@
                     <p>{{ getPropertyAddress }}</p>
                     <p v-if="property.contact_phone" class="margin_40"><a :href="'tel:' + property.contact_phone">{{ property.contact_phone }}</a></p>
                     <div class="footer_links">
-                        <p><a :href="'/pages/'+siteInfo.subdomain+'-privacy-policy/'" target="_blank">Privacy Policy</a></p>
+                        <p><a :href="'/pages/'+siteInfo.subdomain+'-privacy-policy/'" >Privacy Policy</a></p>
                         <!--<a :href="'/pages/'+siteInfo.subdomain+'-code-of-conduct'" target="_blank">Code of Conduct</a> | -->
                     </div>
                 </div>
