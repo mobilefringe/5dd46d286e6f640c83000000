991334cb109e7fefaaed70b64727a57f998dd01c
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-26 11:24:56 -0500

    Bowles Crossing 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -58,13 +58,6 @@
     </div>
 </template>
 
-<style>
-    .mapplic-tooltip-title {
-        color: #fff !important;
-    }
-    
-</style>
-
 <script>
     define(["Vue", "vuex", "vue-select", "vue!search-component", "vue!mapplic-png-map"], function(Vue, Vuex, VueSelect, SearchComponent, MapplicComponent) {
         Vue.component('v-select', VueSelect.VueSelect);
