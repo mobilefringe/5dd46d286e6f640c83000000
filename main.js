049d1db6f7cebed17023046208be4cd9d62d3879
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-01-17 14:03:49 -0500

    Bowles Crossing 1.0  /main.js

--- main.js
+++ main.js
@@ -201,12 +201,6 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
             loadData: async function() {
                 try{
                     await this.$store.dispatch('initializeApi', { site: "bowlescrossing", version: "v4" });
-                    // await this.$store.dispatch('initializeApi', { site: "landing", version: "v4" });
-                    
-                    // to test for jobs
-                    // await this.$store.dispatch('initializeApi', { site: "bramaleacitycentre", version: "v4" });
-                    
-                    
                     await Promise.all([this.$store.dispatch("getData", "property")]);
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     let results = await Promise.all([
