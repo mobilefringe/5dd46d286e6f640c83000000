1878df5b6446674d01531840fb0ae23c6e9ecd70
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-01-17 13:06:53 -0500

    Bowles Crossing 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -25,7 +25,7 @@
                                 <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                             </div>
                             <div v-else>
-                                <a :href="banner.url" target="_blank" aria-label="Go to page for ">
+                                <a :href="banner.url" :aria-label="'Go to page for ' + banner.name + '.'">
                                     <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                                 </a>
                             </div>
@@ -213,7 +213,6 @@
                             }
                         }
                     });
-                    console.log(banners)
                     return banners
                 },
                 featuredItems() {
