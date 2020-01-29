2736a2832df8c8d31e6940b18ce206c94e81083e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-01-29 17:41:45 -0500

    Bowles Crossing 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -103,6 +103,13 @@
                     _.forEach(all_stores, function(value, key) {
                         value.zoom = 2;
                     });
+                    var initZoom = {};
+                    initZoom.svgmap_region = "init";
+                    initZoom.z_coordinate = 1;
+                    initZoom.x = 0.5;
+                    initZoom.y = 0.5;
+                    initZoom.zoom = 1;
+                    all_stores.push(initZoom)
                     return all_stores
                 },
                 allCatergories() {
