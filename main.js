677aa2adf46b95004f1ef9cb21352635a34d7aa3
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-06 16:41:51 -0500

    Bowles Crossing 1.0  /main.js

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                         sub_menu: [
                             {
                                 name: "Directory",
-                                href: "/stores",
+                                href: "/directory",
                                 target: null
                             },
                             {
