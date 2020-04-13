1e19eb13392c09ca4fac5c4f08883b3d15034209
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-04-13 12:27:15 -0400

    Bowles Crossing 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -54,19 +54,16 @@
                                 <div v-masonry-tile  v-for="(store, index) in filteredStores" :key="index" class="stores-grid-item">
                             	    <div class="store_logo_container">
                             	        <router-link :to="'/directory/'+ store.slug">
-                            	        <div v-if="!store.no_store_logo">
+                            	            <div v-if="!store.no_store_logo">
                                 			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                 			    <img  class="store_img" :src="store.store_front_url_abs" :alt="store.name + 'Logo'">
                                 			</div>
-                                			
                                             <div v-else class="no_logo_container">
                                                 <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                                 <div class="no_logo_text">
                                                     <div class="store_text"><h2>{{ store.name }}</h2></div>
                                                 </div>
                                             </div>
-                            	        
-                                	
                                 			<div class="store_tag" v-if="store.total_published_promos">
             									<div class="store_tag_text">Promotion</div>
             								</div>
